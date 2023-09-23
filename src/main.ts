f349f8f1891134ea445f298d4f86e4976242c960
gabrielmpinha <gabriel.mp2000@gmail.com>
2023-09-23 14:48:00 -0300

    enable cors on main

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
